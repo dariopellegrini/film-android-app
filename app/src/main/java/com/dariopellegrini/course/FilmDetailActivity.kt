9f768c6eb305fbb2f7634dbefee423919f8f520b
Dario Pellegrini <dario.pellegrini@s4win.com>
2021-06-16 11:12:17 +0200

    Database

---
+++ app/src/main/java/com/dariopellegrini/course/FilmDetailActivity.kt
@@ -0,0 +1,11 @@
+package com.dariopellegrini.course
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FilmDetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_film_detail)
+    }
+}
---
+++ app/src/main/java/com/dariopellegrini/course/Person.kt
@@ -0,0 +1,4 @@
+package com.dariopellegrini.course
+
+class Person {
+}
